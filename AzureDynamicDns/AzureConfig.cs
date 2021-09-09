a2089701179d8592527ba5e1f2667912fa5a46cc
Jonathan <zxeno@live.com>
2021-09-09 10:31:41 -0700

    initial code commit

---
+++ AzureDynamicDns/AzureConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace AzureDynamicDns
+{
+    using System.Text.Json.Serialization;
+
+    public class AzureConfig
+    {
+        [JsonPropertyName("resourceGroup")]
+        public string ResourceGroup { get; set; }
+        
+        [JsonPropertyName("zoneName")]
+        public string Zone { get; set; }
+        
+        [JsonPropertyName("recordName")]
+        public string Record { get; set; }
+        
+        [JsonPropertyName("subscriptionId")]
+        public string SubscriptionId { get; set; }
+        
+        [JsonPropertyName("tenantId")]
+        public string TenantId { get; set; }
+        
+        [JsonPropertyName("clientId")]
+        public string ClientId { get; set; }
+
+        [JsonPropertyName("clientSecret")]
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ AzureDynamicDns/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace AzureDynamicDns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Http;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Management.Dns;
+    using Microsoft.Azure.Management.Dns.Models;
+    using Microsoft.Rest.Azure.Authentication;
+
+    class Program
+    {
+        private const string JsonFile = "azdns.json";
+
+        public static async Task Main(string[] args)
+        {
+            string expectedFile = Path.Combine(Environment.CurrentDirectory, JsonFile);
+
+            if (!File.Exists(expectedFile))
+            {
+                Console.WriteLine("File not found!");
+                return;
+            }
+
+            string json = File.ReadAllText(expectedFile);
+
+            AzureConfig config = JsonSerializer.Deserialize<AzureConfig>(json);
+
+            await UpdateDNS(config);
+        }
+
+        public static async Task UpdateDNS(AzureConfig options)
+        {
+            var creds = await ApplicationTokenProvider.LoginSilentAsync(options.TenantId, options.ClientId, options.ClientSecret);
+            var dnsClient = new DnsManagementClient(creds);
+            dnsClient.SubscriptionId = options.SubscriptionId;
+
+            var ip = await GetPublicIP();
+            var recordSet = new RecordSet
+            {
+                TTL = 3600,
+                ARecords = new List<ARecord>() { new ARecord(ip) },
+                Metadata = new Dictionary<string, string>()
+                {
+                    { "createdBy", "AzureDynamicDns" },
+                    { "updated", DateTime.Now.ToString() }
+                }
+            };
+
+            var result = await dnsClient.RecordSets.CreateOrUpdateAsync(options.ResourceGroup, options.Zone, options.Record, RecordType.A, recordSet);
+
+            Console.WriteLine(JsonSerializer.Serialize(result, options: new JsonSerializerOptions() { WriteIndented = true }));
+        }
+
+        public static async Task<string> GetPublicIP()
+        {
+            var client = new HttpClient();
+            return await client.GetStringAsync("https://ifconfig.me");
+        }
+    }
+}
