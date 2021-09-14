8d757c37d57e27c466c0210e474b0f140f1d6aa0
Jonathan Cain <zxeno@live.com>
2021-09-13 21:18:57 -0700

    Extended to be more reliable and run repeatedly. Exits on error.

--- AzureDynamicDns/CliOptions.cs
+++ AzureDynamicDns/CliOptions.cs
@@ -4,12 +4,19 @@ namespace AzureDynamicDns
     
     public class CliOptions
     {
-        [Option('c', "configfile", Required = false, HelpText = "Set the config file to use. Otherwise, the default location is used.")]
+        [Option('c', "configfile",
+            Required = false,
+            HelpText = "Set the config file to use. Otherwise, the default location is used. On Windows, this is <%userprofile%\\AppData\\Roaming\\azdns\\azdns.json>. In Linux/Osx, it is <~/.config/azdns/azdns.json>")]
         public string ConfigFile { get; set; }
 
-        [Option('r', "rate", Required = false, HelpText = "The rate in seconds at which to update. Default is 600 seconds (10 minutes).")]
+        [Option('r', "rate",
+            Required = false,
+            HelpText = "The rate in seconds at which to update. Default is 600 seconds (10 minutes).")]
         public int Rate { get; set; } = 600;
         
-        
+        [Option('l', "logfile",
+        Required = false,
+        HelpText = "This parameter provides two functions in one: Indicates that a logfile should be written, and where to write said logfile. If the value 'default' is provided, then the working directory is used. Otherwise, specify a log file location and sure that the application has write access to that location.")]
+        public string LogFile { get; set; }
     }
 }
--- AzureDynamicDns/Program.cs
+++ AzureDynamicDns/Program.cs
@@ -1,69 +1,94 @@
 ﻿namespace AzureDynamicDns
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
-    using System.Net.Http;
+    using System.Runtime.InteropServices;
     using System.Text.Json;
+    using System.Threading;
     using System.Threading.Tasks;
     using CommandLine;
-    using Microsoft.Azure.Management.Dns;
-    using Microsoft.Azure.Management.Dns.Models;
-    using Microsoft.Rest.Azure.Authentication;
 
     public class Program
     {
-        private const string JsonFile = "azdns.json";
+        private const string ConfigFileName = "azdns.json";
+        private static SimplisticLogger logger = null;
 
         public static async Task Main(string[] args)
         {
+            // Parse our options first. This library allows us to pass parsed arguments directly to
+            // a function, but I want to use those manually before initiating the primary program
+            // functionality.
             CliOptions options = null;
-
             Parser.Default.ParseArguments<CliOptions>(args).WithParsed((o) => { options = o; });
+            
+            // Activate the basic logger
+            logger = new SimplisticLogger(options.LogFile);
+            logger.Init();
 
-            string filePath = string.IsNullOrEmpty(options.ConfigFile) ?
-                Path.Combine(Environment.CurrentDirectory, JsonFile) : options.ConfigFile;
-
-            if (!File.Exists(filePath))
+            // Check for config file parameter. If the path provided by command line is null,
+            // then check for one by OS location.
+            if (string.IsNullOrEmpty(options.ConfigFile))
             {
-                Console.WriteLine("File not found!");
+                await logger.LogInfoAsync("No config file path was provided Using default.");
+                
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                    || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    options.ConfigFile = Path.Combine(Environment.SpecialFolder.UserProfile.ToString(), "/.config/azdns/", ConfigFileName);
+                }
+                else
+                {
+                    options.ConfigFile = Path.Combine(Environment.SpecialFolder.UserProfile.ToString(), "\\AppData\\azdns\\", ConfigFileName);
+                }
+            }
+
+            // Ensure that config exists. 
+            if (!File.Exists(options.ConfigFile))
+            {
+                await logger.LogErrorAsync($"Could not find a config file at {options.ConfigFile}");
+                
+                await logger.LogInfoAsync("Configuration file not found!");
+                try
+                {
+                    await File.WriteAllTextAsync(options.ConfigFile, AzureConfig.CreateJsonTemplate());
+                }
+                catch (Exception e)
+                {
+                    await logger.LogErrorAsync($"Unable to create configuration template:\n{e.Message}");
+                    await logger.LogInfoAsync("\nA configuration template should be in the working directory of this program by default. You may copy that and provide it vay the '-c' parameter like so: \"/path/to/executable -c /path/to/your/config_file.json\"", true);
+                }
+
+                await logger.LogInfoAsync($"A configuration file is required. A blank one has been created and placed at {options.ConfigFile}");
                 return;
             }
 
-            string json = await File.ReadAllTextAsync(filePath);
-            AzureConfig config = JsonSerializer.Deserialize<AzureConfig>(json);
-
-            await UpdateDns(config);
-        }
-
-        private static async Task UpdateDns(AzureConfig options)
-        {
-            var creds = await ApplicationTokenProvider.LoginSilentAsync(options.TenantId, options.ClientId, options.ClientSecret);
-            var dnsClient = new DnsManagementClient(creds);
-            dnsClient.SubscriptionId = options.SubscriptionId;
-
-            var ip = await GetPublicIp();
-            var recordSet = new RecordSet
+            // Read and parse config
+            AzureConfig config = null;
+            try
             {
-                TTL = 3600,
-                ARecords = new List<ARecord>() { new ARecord(ip) },
-                Metadata = new Dictionary<string, string>()
+                string json = await File.ReadAllTextAsync(options.ConfigFile);
+                config = JsonSerializer.Deserialize<AzureConfig>(json) ?? throw new Exception("Deserializer returned a null result somehow.");
+                config.ConfigFilePath = options.ConfigFile;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to parse configuration file!\n{e.Message}");
+            }
+
+            DnsService dns = new DnsService(config, logger);
+            while (true)
+            {
+                try
                 {
-                    { "createdBy", "AzureDynamicDns" },
-                    { "updated", DateTime.Now.ToString(CultureInfo.InvariantCulture) }
+                    await dns.UpdateDns();
+                    Thread.Sleep(options.Rate * 1000);
                 }
-            };
-
-            var result = await dnsClient.RecordSets.CreateOrUpdateAsync(options.ResourceGroup, options.Zone, options.Record, RecordType.A, recordSet);
-
-            Console.WriteLine(JsonSerializer.Serialize(result, options: new JsonSerializerOptions() { WriteIndented = true }));
-        }
-
-        private static async Task<string> GetPublicIp()
-        {
-            var client = new HttpClient();
-            return await client.GetStringAsync("https://ifconfig.me");
+                catch (Exception e)
+                {
+                    await logger.LogErrorAsync($"Exiting due to error:\n{e.Message}");
+                    break;
+                }
+            }
         }
     }
 }
